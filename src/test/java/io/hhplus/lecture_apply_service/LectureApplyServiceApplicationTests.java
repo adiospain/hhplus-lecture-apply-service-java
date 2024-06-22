817084482a8ff29895f55778e99db313d326ea56
hyeonwoody <hyeonwoody@gmail.com>
2024-06-22 20:38:56 +0900

    🎉INIT : init commit

---
+++ src/test/java/io/hhplus/lecture_apply_service/LectureApplyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.lecture_apply_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
