d028ec92d4ba5021129a5e32c700e54466c44603
hyeonwoody <hyeonwoody@gmail.com>
2024-06-23 17:58:36 +0900

    CustomException 구현

---
+++ src/main/java/io/hhplus/lecture_apply_service/exception/ErrorCode.java
@@ -0,0 +1,28 @@
+package io.hhplus.lecture_apply_service.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    //400
+
+    INVALID_LECTURE_ID(BAD_REQUEST, "유효하지 않은 특강 ID 입니다."),
+    INVALID_USER_ID(BAD_REQUEST, "유효하지 않은 유저 ID 입니다."),
+
+    //404
+
+
+    //409
+
+    //500
+
+    DUMMY(BAD_REQUEST, "Dummy");
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
