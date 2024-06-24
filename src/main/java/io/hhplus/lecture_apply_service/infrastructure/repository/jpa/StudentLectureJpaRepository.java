175a2382eebabe7b9b5323aeef013bb069eb190b
hyeonwoody <hyeonwoody@gmail.com>
2024-06-25 03:53:53 +0900

    특강 신청 여부 조회 (EnrolledLectureUseCase) 추가

--- src/main/java/io/hhplus/lecture_apply_service/infrastructure/repository/jpa/StudentLectureJpaRepository.java
+++ src/main/java/io/hhplus/lecture_apply_service/infrastructure/repository/jpa/StudentLectureJpaRepository.java
@@ -2,6 +2,7 @@ package io.hhplus.lecture_apply_service.infrastructure.repository.jpa;
 
 import io.hhplus.lecture_apply_service.infrastructure.entity.StudentLectureJpaEntity;
 import jakarta.persistence.LockModeType;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 
@@ -9,4 +10,6 @@ public interface StudentLectureJpaRepository extends JpaRepository<StudentLectur
 
 
   boolean existsByStudentIdAndLectureIdAndEnrollmentIsTrue(Long studentId, Long lectureId);
+
+  List<StudentLectureJpaEntity> findAllByStudentId(Long studentId);
 }
