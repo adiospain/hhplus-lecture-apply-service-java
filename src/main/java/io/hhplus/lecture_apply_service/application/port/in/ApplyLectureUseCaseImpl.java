baf656e8a352b4f7e4dcce8fff01a97bae7272c3
hyeonwoody <hyeonwoody@gmail.com>
2024-06-25 00:47:08 +0900

    ApplyLectureUseCase 리팩토링 및 예외처리

--- src/main/java/io/hhplus/lecture_apply_service/application/port/in/ApplyLectureUseCaseImpl.java
+++ src/main/java/io/hhplus/lecture_apply_service/application/port/in/ApplyLectureUseCaseImpl.java
@@ -1,81 +1,86 @@
 package io.hhplus.lecture_apply_service.application.port.in;
 
 import io.hhplus.lecture_apply_service.application.port.out.LectureLock;
-import io.hhplus.lecture_apply_service.infrastructure.entity.StudentJpaEntity;
-import io.hhplus.lecture_apply_service.infrastructure.entity.StudentLectureJpaEntity;
+import io.hhplus.lecture_apply_service.infrastructure.entity.Student;
+
 import io.hhplus.lecture_apply_service.common.UseCase;
-import io.hhplus.lecture_apply_service.infrastructure.entity.LectureJpaEntity;
+import io.hhplus.lecture_apply_service.infrastructure.entity.Lecture;
 import io.hhplus.lecture_apply_service.exception.CustomException;
+import io.hhplus.lecture_apply_service.infrastructure.entity.StudentLecture;
 import io.hhplus.lecture_apply_service.infrastructure.repository.LectureRepository;
 import io.hhplus.lecture_apply_service.infrastructure.repository.StudentLectureRepository;
 import io.hhplus.lecture_apply_service.infrastructure.repository.StudentRepository;
 import io.hhplus.lecture_apply_service.presentation.dto.res.ApplyLectureAPIResponse;
 import io.hhplus.lecture_apply_service.exception.ErrorCode;
-import java.util.Optional;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.concurrent.Semaphore;
 
 
 @RequiredArgsConstructor
 @UseCase
 public class ApplyLectureUseCaseImpl implements ApplyLectureUseCase {
 
-    private final LectureLock lectureLock;
     private final LectureRepository lectureRepository;
     private final StudentRepository studentRepository;
     private final StudentLectureRepository studentLectureRepository;
 
-    private final Semaphore semaphore = new Semaphore(1);
-
-
-
-    @Transactional
-    public ApplyLectureAPIResponse executeNope(ApplyLectureCommand command) {
-        try{
-            //semaphore.acquire();
-            LectureJpaEntity lectureJpaEntity = lectureRepository.findByIdxLock(command.getLectureId())
-                .orElseThrow(()->
-                    new CustomException(ErrorCode.INVALID_LECTURE_ID));
-            if (lectureJpaEntity.getCapacity() <= 0){
-                return new ApplyLectureAPIResponse(command.getStudentId(), command.getLectureId(), false);
-            }
-            lectureJpaEntity.setCapacity(lectureJpaEntity.getCapacity()-1);
-            lectureRepository.savexLock(lectureJpaEntity);
-            return new ApplyLectureAPIResponse(command.getStudentId(), command.getLectureId(), true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }finally {
-            //semaphore.release();
-        }
-    }
-
     @Override
-    @Transactional
-    public ApplyLectureAPIResponse execute(ApplyLectureCommand command) {
-        try{
-            //semaphore.acquire();
-            LectureJpaEntity lectureJpaEntity = lectureRepository.findById(command.getLectureId())
-                    .orElseThrow(()->
-                            new CustomException(ErrorCode.INVALID_LECTURE_ID));
-            if (lectureJpaEntity.getCapacity() <= 0){
-                return new ApplyLectureAPIResponse(command.getStudentId(), command.getLectureId(), false);
-            }
+    public ApplyLectureAPIResponse execute (ApplyLectureCommand command) {
+      //유효한 특강ID 확인
+      Lecture lecture = validateLecture(command.getLectureId());
+      //유효한 학생ID 확인
+      Student student = validateStudent(command.getStudentId());
+      //이미 수강한 특강인지 확인
+      validateNotAlreadyApplied(student.getId(), command.getLectureId());
 
-            StudentJpaEntity studentJpaEntity = studentRepository.findById(command.getStudentId())
-                    .orElseThrow(()->
-                            new CustomException(ErrorCode.INVALID_STUDENT_ID));
+      //수강 인원 확인
+      ApplyLectureAPIResponse response = handleLectureCapacity(student, lecture);
+      if (response != null) {
+        //수강 인원 초과로 수강 실패
+        saveApplyHistory(student,lecture,false);
+        return response;
+      }
 
-            lectureJpaEntity.setCapacity(lectureJpaEntity.getCapacity()-1);
-            lectureRepository.save(lectureJpaEntity);
-            StudentLectureJpaEntity studentLecture = new StudentLectureJpaEntity(studentJpaEntity, lectureJpaEntity);
-            studentLectureRepository.save(studentLecture);
-            return new ApplyLectureAPIResponse(command.getStudentId(), command.getLectureId(), true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }finally {
-            //semaphore.release();
-        }
+      if (LocalDateTime.now().isBefore(lecture.getOpen_at())){
+        throw new CustomException(ErrorCode.INVALID_TIME);
+      }
+
+      //수강인원 감소
+      lecture.setCapacity(lecture.getCapacity()-1);
+      lectureRepository.savexLock(lecture);
+
+      //수강신청 성공 히스토리 추가
+      saveApplyHistory(student,lecture,true);
+      return new ApplyLectureAPIResponse(command.getStudentId(), command.getLectureId(), true);
     }
+
+  private void saveApplyHistory(Student student, Lecture lecture, boolean enrollment) {
+    StudentLecture studentLecture = new StudentLecture(student, lecture, enrollment);
+    studentLectureRepository.save(studentLecture);
+  }
+
+  private Lecture validateLecture(Long lectureId) {
+      Lecture lecture = lectureRepository.findByIdxLock(lectureId)
+          .orElseThrow(()-> new CustomException(ErrorCode.INVALID_LECTURE_ID));
+      if (LocalDateTime.now().isBefore(lecture.getOpen_at())){
+        throw new CustomException(ErrorCode.INVALID_TIME);
+      }
+      return lecture;
+  }
+  private void validateNotAlreadyApplied(Long studentId, Long lectureId) {
+    if (studentLectureRepository.existsByStudentIdAndLectureIdAndEnrollmentIsTrue(studentId, lectureId)) {
+      throw new CustomException(ErrorCode.ALREADY_APPLIED);
+    }
+  }
+
+  private Student validateStudent(Long studentId) {
+    return studentRepository.findById(studentId)
+        .orElseThrow(() -> new CustomException(ErrorCode.INVALID_STUDENT_ID));
+  }
+
+  private ApplyLectureAPIResponse handleLectureCapacity(Student student, Lecture lecture) {
+      return lecture.getCapacity() == 0 ? new ApplyLectureAPIResponse(student.getId(), lecture.getId(), false) : null;
+  }
+
+
 }
